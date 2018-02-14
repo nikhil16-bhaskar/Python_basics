8c955bcaa553952d09597202d923c7c2ae30ceb2
nikhil bhaskar <perfectnikhil1697@gmail.com>
2018-02-14 23:42:20 +0530

    anagram code added

---
+++ anagram.py
@@ -0,0 +1,10 @@
+word1=input("enter word 1")
+word2=input("enter word 2")
+sorted_word1=sorted(word1)
+sorted_word2=sorted(word2)
+if sorted_word1==sorted_word2:
+    print("Anagram")
+else:
+    print ("Not Anagram")
+        
+        
